e1969b51b770b6add6ee354c9098d9b753a229eb
Sucerquia <dsucerg@gmail.com>
2024-05-15 09:33:07 +0200

    ENH: unified BDEs

--- analysis/BDES.py
+++ analysis/BDES.py
@@ -1,26 +1,55 @@
 from myutils.miscellaneous import output_terminal
 import sys
 
-subdir = sys.argv[1]
+
+kind = int(sys.argv[1]) # 1 or 2
+sw = sys.argv[2] # orga or g09
+
+if kind == 1:
+    whole = '0_0-BeforeRupture'
+    num = ''
+elif kind == 2:
+    whole = '0_1-BeforeRupture_wo_extRings'
+    num = '2'
+
+if sw == 'orca':
+    keyword = "Total enthalpy"
+    awk = "awk '{print $4}'"
+    sw = ''
+    output = 'BDE.out'
+elif sw == 'g09':
+    keyword = "Enthalpies"
+    awk = "awk '{print $7}'"
+    output = 'g09BDE.log'
+else:
+    raise ValueError("Not recognized software")
 
 # complet system
-E_com = output_terminal("grep 'Total enthalpy' "
-                        f"../0_0-BeforeRupture/{subdir}/BDE.out" + " | awk '{print $4}'", print_output=False)
+E_com = output_terminal(f"grep '{keyword}' "
+                        f"../{whole}/BDE{num}{sw}/{output}"
+                        f" | {awk}",
+                        print_output=False)
 
 # triple bond brocken
-E_t12 = output_terminal("grep 'Total enthalpy' "
-                        f"../7_0-triple_broken/{subdir}/BDE.out" + " | awk '{print $4}'", print_output=False)
+E_t12 = output_terminal(f"grep '{keyword}' "
+                        f"../7_0-triple_broken/BDE{num}{sw}/{output}"
+                        f" | {awk}",
+                        print_output=False)
 
 deltaE = 2 * float(E_t12) -  float(E_com)
 print('BDE triple bond: ', deltaE, " Ha / ", deltaE * 2600,  " kJ/mol")
 
 # single
-E_si1 = output_terminal("grep 'Total enthalpy' "
-                        f"../6_0-primary_in_triple/{subdir}/BDE.out" + " | awk '{print $4}'", print_output=False)
+E_si1 = output_terminal(f"grep '{keyword}' "
+                        f"../6_0-primary_in_triple/BDE{num}{sw}/{output}"
+                        f" | {awk}",
+                        print_output=False)
 
 # triple bond brocken
-E_si2 = output_terminal("grep 'Total enthalpy' "
-                        f"../5_0-primary_in_ring/{subdir}/BDE.out" + " | awk '{print $4}'", print_output=False)
+E_si2 = output_terminal(f"grep '{keyword}' "
+                        f"../5_0-primary_in_ring/BDE{num}{sw}/{output}"
+                        f" | {awk}",
+                        print_output=False)
 
 deltaE = float(E_si1) + float(E_si2) - float(E_com)
 print('BDE single bond: ', deltaE, " Ha / ", deltaE * 2600,  " kJ/mol")
